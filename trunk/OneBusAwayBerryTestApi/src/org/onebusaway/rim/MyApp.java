2e542d59ba73806d3890af400a99a40b786ed784
pv@swooby.com <pv@swooby.com@84bad66c-4ae1-db43-f5f2-1b4f98d84025>
2011-01-12 01:20:28 +0000

    Share project "OneBusAwayBerryTestApi" into "https://onebusaway-berry.googlecode.com/svn"

---
+++ trunk/OneBusAwayBerryTestApi/src/org/onebusaway/rim/MyApp.java
@@ -0,0 +1,39 @@
+//#preprocess
+
+package org.onebusaway.rim;
+
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.RichTextField;
+import net.rim.device.api.ui.container.MainScreen;
+
+public class MyApp extends UiApplication
+{
+    public static void main(String[] args)
+    {
+        MyApp theApp = new MyApp();
+        theApp.enterEventDispatcher();
+    }
+
+    public MyApp()
+    {
+        pushScreen(new MyMainScreen());
+    }
+
+    public static void log(String msg)
+    {
+        System.out.println(msg);
+    }
+
+    class MyMainScreen extends MainScreen
+    {
+        protected RichTextField rtf;
+
+        public MyMainScreen()
+        {
+            super(NO_HORIZONTAL_SCROLL | NO_VERTICAL_SCROLL);
+
+            rtf = new RichTextField();
+            add(rtf);
+        }
+    }
+}
